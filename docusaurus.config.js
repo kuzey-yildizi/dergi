496064bfaa9ed83dd36a919fcde389ba1a5c24e6
MuratHaktanir <mhaktanir@icloud.com>
2024-08-16 12:49:36 +0300

    base url değişti.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,10 +14,10 @@ const config = {
   favicon: 'img/ky01_01_atayuludokumaci.png',
 
   // Set the production url of your site here
-  url: 'https://monobilisim.github.io/',
+  url: 'https://kuzey-yildizi.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-kuzeyyildizi/',
+  baseUrl: '/dergi/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'monobilisim', // Usually your GitHub org/user name.
